4c98baf7b2174ee3bf11fb130f1eccf0d4129e6a
Tesco <lucaforprez@web.de>
2019-11-28 12:38:29 +0100

    finished svmclassifier

--- classifiers/SimpleSVMClassifier.py
+++ classifiers/SimpleSVMClassifier.py
@@ -14,7 +14,5 @@ class SimpleSVMClassifier: # < handle
     def train(self, x, y, trainingWeights):
         self.model.fit(x, y, trainingWeights)
 
-    #score ist prediction score des Klassifizierers, label ist actual class label
     def predict(self, x):
-        [label, score] = self.model.predict(self.model, x)
-        return label * np.abs(score[0]) + (1-label) * (1-np.abs(score[0]))
+        return self.model.predict(self.model, x)
