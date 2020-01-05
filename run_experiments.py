c01d7f14e05a5d27d5c75761af52f7c5771b0cf0
M-Jahnke <marcel.jahnke@gmx.de>
2020-01-05 13:00:49 +0100

    comments

--- run_experiments.py
+++ run_experiments.py
@@ -3,9 +3,9 @@ from run_dataset_exp import run_dataset_exp
 def main():
     # run_dataset_exp("compass", "compass.logs", 5)           # works, paper result were replicated!
     # run_dataset_exp("bank", "bank.logs", 5)                 # works, pRule was 0.04 lower than in the paper!
-    run_dataset_exp("adult", "adult_census.logs", 5)      # needs testing (with less rows)
-    run_dataset_exp("kdd","kdd_census.logs",5)            # needs testing (with less rows, but more iterations)
-    run_dataset_exp("dutch", "dutch.logs", 5)             # needs testing (with less rows, but more iterations)
+    run_dataset_exp("adult", "adult_census.logs", 5)      # works, pRule only 0.03 lower, acc 0.135 lower!
+    run_dataset_exp("kdd","kdd_census.logs",5)            # works
+    run_dataset_exp("dutch", "dutch.logs", 5)             # works
     # run_dataset_exp("synth_opp", "opp.logs", 5, True)     # works, replicated for avoid disparate treatment!
     # run_dataset_exp("synth_same", "same.logs", 5, True)   # works, replicated for avoid disparate treatment!
     # run_dataset_exp("synth_opp", "opp_dt.logs", 5, False)   # works, acc is 0.05 lower than in the paper!
